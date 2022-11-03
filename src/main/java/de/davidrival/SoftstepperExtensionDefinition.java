ab2b7d509dcad19822e4cc27f42228d9649febf9
David Rival <systemexklusiv@gmail.com>
2022-11-03 15:16:09 +0100

    initial

---
+++ src/main/java/de/davidrival/SoftstepperExtensionDefinition.java
@@ -0,0 +1,99 @@
+package de.davidrival;
+import java.util.UUID;
+
+import com.bitwig.extension.api.PlatformType;
+import com.bitwig.extension.controller.AutoDetectionMidiPortNamesList;
+import com.bitwig.extension.controller.ControllerExtensionDefinition;
+import com.bitwig.extension.controller.api.ControllerHost;
+
+public class SoftstepperExtensionDefinition extends ControllerExtensionDefinition
+{
+   private static final UUID DRIVER_ID = UUID.fromString("3f41be72-fe05-4fc6-88a7-a682f033bf61");
+   
+   public SoftstepperExtensionDefinition()
+   {
+   }
+
+   @Override
+   public String getName()
+   {
+      return "BWSSoftstepper";
+   }
+   
+   @Override
+   public String getAuthor()
+   {
+      return "David Rival systemexklusiv";
+   }
+
+   @Override
+   public String getVersion()
+   {
+      return "0.1";
+   }
+
+   @Override
+   public UUID getId()
+   {
+      return DRIVER_ID;
+   }
+   
+   @Override
+   public String getHardwareVendor()
+   {
+      return "AAA";
+   }
+   
+   @Override
+   public String getHardwareModel()
+   {
+      return "BWSSoftstepper";
+   }
+
+   @Override
+   public int getRequiredAPIVersion()
+   {
+      return 17;
+   }
+
+   @Override
+   public int getNumMidiInPorts()
+   {
+      return 1;
+   }
+
+   @Override
+   public int getNumMidiOutPorts()
+   {
+      return 1;
+   }
+
+   @Override
+   public void listAutoDetectionMidiPortNames(final AutoDetectionMidiPortNamesList list, final PlatformType platformType)
+   {
+      if (platformType == PlatformType.WINDOWS)
+      {
+         // TODO: Set the correct names of the ports for auto detection on Windows platform here
+         // and uncomment this when port names are correct.
+         // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
+      }
+      else if (platformType == PlatformType.MAC)
+      {
+         // TODO: Set the correct names of the ports for auto detection on Windows platform here
+         // and uncomment this when port names are correct.
+         // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
+      }
+      else if (platformType == PlatformType.LINUX)
+      {
+         // TODO: Set the correct names of the ports for auto detection on Windows platform here
+         // and uncomment this when port names are correct.
+         // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
+      }
+   }
+
+   @Override
+   public SoftstepperExtension createInstance(final ControllerHost host)
+   {
+      return new SoftstepperExtension(this, host);
+   }
+}
